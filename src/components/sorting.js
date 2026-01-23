838a3bdc073bb39055f863637530a7a47f69cef4
iadoreplato <sidneva.d.v@gmail.com>
2026-01-23 09:59:13 +0300

    1st

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,32 @@
 import {sortCollection, sortMap} from "../lib/sort.js";
-
-export function initSorting(columns) {
-    return (data, state, action) => {
+export function initSorting(columns) { //инициализирует сортировку и возвращает функцию, которое принимает данные, состояние и действие и возвращает отсортированный массив
+    return (data, state, action) => { //columns - массив кнопок сортировки 
         let field = null;
         let order = null;
 
         if (action && action.name === 'sort') {
             // @todo: #3.1 — запомнить выбранный режим сортировки
+       action.dataset.value = sortMap[action.dataset.value];
+        field = action.dataset.field;
+        order = action.dataset.value;
 
             // @todo: #3.2 — сбросить сортировки остальных колонок
-        } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+            columns.forEach(column => {                                    // Перебираем элементы (в columns у нас массив кнопок)
+    if (column.dataset.field !== action.dataset.field) {    // Если это не та кнопка, что нажал пользователь
+        column.dataset.value = 'none';                        // тогда сбрасываем её в начальное состояние
+    }
+});
+        }  else {
+            // если кнопка не нажата, ищем активную колонку
+            columns.forEach(column => {
+                if (column.dataset.value !== 'none') {
+                    field = column.dataset.field;
+                    order = column.dataset.value;
+                }
+            });
         }
 
+        // возвращаем отсортированный массив
         return sortCollection(data, field, order);
-    }
+    };
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,5 +1,5 @@
-import {cloneTemplate} from "../lib/utils.js";
 
+import { cloneTemplate } from "../lib/utils";
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
  *
@@ -9,17 +9,68 @@ import {cloneTemplate} from "../lib/utils.js";
  */
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
+    //root — это один объект-контейнер для модуля таблицы, внутри которого могут храниться клонированные шаблоны.
     const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы  
+    root.beforeTemplates = {};
+    root.afterTemplates = {};
+
+before.reverse().forEach((id) => {
+    //<template id="table">...</template>
+   root[id] = cloneTemplate(id);
+   root.container.prepend(root[id].container); 
+   root.beforeTemplates[id] = root[id]
+   })
+
+after.forEach((id) => {
+   root[id] = cloneTemplate(id);
+   root.container.appendChild(root[id].container); 
+   root.afterTemplates[id] = root[id];
+   })
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+   root.container.addEventListener('change', (e) => {
+    onAction();
+   })
+   
+   root.container.addEventListener('reset', (e) => {
+    setTimeout(onAction);
+   })
+
+   root.container.addEventListener('submit', (e) => {
+   e.preventDefault();
+   onAction(e.submitter);
+   })
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
+         const nextRows = data.map(item => { 
+            const row = cloneTemplate(rowTemplate); //возвращает объект с контейнером (клоном шаблона) и именованными элементами (data-name)
+            //берёт объект и возвращает массив его собственных ключей
+            Object.keys(item).forEach(key => { 
+                //проверяем, существует ли определенный ключ в data-name в шаблоне
+                if (row.elements[key]) {
+                    //если он есть, присваиваем ему данные опрделенного продаца по заданному ключу
+                   row.elements[key].textContent = item[key];
+                }
+            });
+            //root.container - это ключ из объекта - клонированный элемент, вся таблица 
+            return row.container
+         });
+         //root.elements - ключ из объекта соотвествующий data-name: значение
+         root.elements.rows.replaceChildren(...nextRows);
     }
-
-    return {...root, render};
+        return {...root, render};
 }
+/* root — не DOM-узел, а объект с двумя ключевыми свойствами:
+	1.	container → сам DOM-элемент (всё, что клонировали из tableTemplate)
+	2.	elements → объект с DOM-узлами внутри шаблона, определяемыми по data-name
+    root = {
+  container: <form class="table">…</form>, // DOM-узел всей таблицы
+  elements: {
+    rows: <div class="table-content" data-name="rows"></div>,  // контейнер для строк
+    header: <div class="table-row header-row">…</div>,
+    filter: <div class="table-row filter-row">…</div>
+  }
+} */

2d5148ddaf6c7a22c66dbae3351ec1bafb1364b5
iadoreplato <sidneva.d.v@gmail.com>
2026-02-12 15:37:47 +0300

    4th  commit

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -4,17 +4,17 @@ export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) =
 
 let pageCount;
 const applyPagination = (query, state, action) => {
-    const limit = state.rowsPerPage;
-    let page = state.page;
+    const limit = Number(state.rowsPerPage) || 10;
+    let page = Number(state.page) || 1;
 
-    // переносим код, который делали под @todo: #2.6
-if (action) switch(action.name) {
-    case 'prev': page = Math.max(1, page - 1); break;            // переход на предыдущую страницу
-    case 'next': page = Math.min(pageCount, page + 1); break;    // переход на следующую страницу
-    case 'first': page = 1; break;                                // переход на первую страницу
-    case 'last': page = pageCount; break;                        // переход на последнюю страницу
-}
-    return Object.assign({}, query, { // добавим параметры к query, но не изменяем исходный объект
+    if (action) switch(action.name) {
+        case 'prev': page = Math.max(1, page - 1); break;
+        case 'next': page = Math.min(pageCount, page + 1); break;
+        case 'first': page = 1; break;
+        case 'last': page = pageCount; break;
+    }
+
+    return Object.assign({}, query, {
         limit,
         page
     });
@@ -25,7 +25,7 @@ const updatePagination = (total, { page, limit }) => {
 
     // переносим код, который делали под @todo: #2.4
     const pageTemplate = pages.firstElementChild.cloneNode(true); 
-    const visiblePages = getPages(page, pageCount, 5);                // Получим массив страниц, которые нужно показать, выводим только 5 страниц
+    const visiblePages = getPages(page, pageCount, 5);      
     pages.replaceChildren(...visiblePages.map(pageNumber => {        // перебираем их и создаём для них кнопку
         const el = pageTemplate.cloneNode(true);                    // клонируем шаблон, который запомнили ранее
         return createPage(el, pageNumber, pageNumber === page);        // вызываем колбэк из настроек, чтобы заполнить кнопку данными
